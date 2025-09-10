b48244c35ca76e150e70e544f97fa98af3f35962
docsuri <docsuri@tutamail.com>
2025-09-10 17:15:38 +0900

    add cask: font-hcr-lvt

---
+++ Casks/font-hcr-lvt.rb
@@ -0,0 +1,18 @@
+# Documentation: https://docs.brew.sh/Cask-Cookbook
+#                https://docs.brew.sh/Adding-Software-to-Homebrew#cask-stanzas
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+cask "font-hcr-lvt" do
+  version "1.981"
+  sha256 ""
+
+  url "https://mirror.ischo.org/KTUG/texlive/tlnet/archive/hcr-lvt.tar.xz"
+  name "font-hcr-lvt"
+  desc ""
+  homepage "https://mirror.ischo.org/KTUG/texlive/tlnet/archive/"
+
+  font "hcr-lvt/fonts/truetype/hancom/hcr-lvt/HANBatang-LVT.ttf"
+  font "hcr-lvt/fonts/truetype/hancom/hcr-lvt/HANBatangB-LVT.ttf"
+  font "hcr-lvt/fonts/truetype/hancom/hcr-lvt/HANDotum-LVT.ttf"
+  font "hcr-lvt/fonts/truetype/hancom/hcr-lvt/HANDotumB-LVT.ttf"
+
+end
