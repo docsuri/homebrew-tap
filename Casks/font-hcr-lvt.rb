21e6b0265587d43b118995b491a28ab18da8920e
docsuri <docsuri@tutamail.com>
2025-09-10 17:22:50 +0900

    fix cask: font-hcr-lvt font path

--- Casks/font-hcr-lvt.rb
+++ Casks/font-hcr-lvt.rb
@@ -3,16 +3,16 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 cask "font-hcr-lvt" do
   version "1.981"
-  sha256 ""
+  sha256 "3aaddd80eddd00a1c5f92d550999642dd711f2a2b59956ced39e33de0bbc6d51"
 
   url "https://mirror.ischo.org/KTUG/texlive/tlnet/archive/hcr-lvt.tar.xz"
   name "font-hcr-lvt"
-  desc ""
+  desc "The Korean TeX Society has added GSUB/GPOS/vhea/vmtx tables chiefly for old hangul rendering. Please contact http://www.ktug.org for these issues."
   homepage "https://mirror.ischo.org/KTUG/texlive/tlnet/archive/"
 
-  font "hcr-lvt/fonts/truetype/hancom/hcr-lvt/HANBatang-LVT.ttf"
-  font "hcr-lvt/fonts/truetype/hancom/hcr-lvt/HANBatangB-LVT.ttf"
-  font "hcr-lvt/fonts/truetype/hancom/hcr-lvt/HANDotum-LVT.ttf"
-  font "hcr-lvt/fonts/truetype/hancom/hcr-lvt/HANDotumB-LVT.ttf"
+  font "fonts/truetype/hancom/hcr-lvt/HANBatang-LVT.ttf"
+  font "fonts/truetype/hancom/hcr-lvt/HANBatangB-LVT.ttf"
+  font "fonts/truetype/hancom/hcr-lvt/HANDotum-LVT.ttf"
+  font "fonts/truetype/hancom/hcr-lvt/HANDotumB-LVT.ttf"
 
 end
